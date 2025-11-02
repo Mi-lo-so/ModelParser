5886d3d90069b3586fd03089d4ac1f2154635913
Mi-lo-so <mialsogaard@gmail.com>
2025-11-03 00:23:44 +0100

    Add files via upload

--- ModelParserWebApp/Modules/DynamoDbWebAppExtensions.cs
+++ ModelParserWebApp/Modules/DynamoDbWebAppExtensions.cs
@@ -7,14 +7,6 @@ namespace ModelParserApp.Modules;
 /// </summary>
 public static class DynamoDbWebAppExtensions
 {
-    // interfaces - fordi de er nemmere at teste, og selvfolgelig nemmere at instantiere paa
-    // using the singleton dependency injection for the dynamotable using the table name ensures we could have multiple tables.
-    // SectionName er required by IOptions. Indbygget i C#. laver til model
-    // TODO check what happens if unique id is already in DB
-    // reconsider modelInfo.cs dynamo specification - more generic?
-    // Guard framework??
-    // TODO more expertise on DynamoDB
-    // TODO after removing thr DynamoDbInitializer, I can remove the dependency on dot net dependency in ModelParseApp
     public static void InitializeDynamoDb(this WebApplication app)
     {
         using (var scope = app.Services.CreateScope())
